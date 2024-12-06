44277209e7b9789edd7680cd52fda32e1be3b921
sapan singh <sapansinghchouhan@gmail.com>
2024-12-06 20:34:01 +0530

    .

--- src/main/java/com/inspection/penalty/component/ApiScheduler.java
+++ src/main/java/com/inspection/penalty/component/ApiScheduler.java
@@ -1,41 +1,46 @@
-package com.inspection.penalty.component;
+// package com.inspection.penalty.component;
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.scheduling.annotation.Async;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.client.RestTemplate;
 
-@Component
-public class ApiScheduler {
+// @Component
+// public class ApiScheduler {
 
-    private final RestTemplate restTemplate;
+//     private final RestTemplate restTemplate;
 
-    // Inject RestTemplate or create a bean for it
-    public ApiScheduler(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+//     // Inject RestTemplate or create a bean for it
+//     public ApiScheduler(RestTemplate restTemplate) {
+//         this.restTemplate = restTemplate;
+//     }
 
-    //@Scheduled(cron = "0 0/45 * * * ?") // Cron expression for every 45 minutes
-    //
-    @Scheduled(fixedRate = 3000000)
-    public void callApiEvery45Minutes() {
-        String url = "http://192.168.200.10/pushservice/tokengerator.php"; // Replace with your API URL
-        try {
-            String response = restTemplate.getForObject(url, String.class);
-            System.out.println("API response: " + response);
-        } catch (Exception e) {
-            System.err.println("Error calling the API: " + e.getMessage());
-        }
-    }
+//     //@Scheduled(cron = "0 0/45 * * * ?") // Cron expression for every 45 minutes
+//     //
+//     @Async
+//     @Scheduled(fixedRate = 3000000)
+//     public void callApiEvery45Minutes() {
+//         String url = "http://192.168.200.10/pushservice/tokengerator.php"; // Replace with your API URL
+//         try {
+//             String response = restTemplate.getForObject(url, String.class);
+//             System.out.println("API response: " + response);
+//             Thread.sleep(3000);
+//         } catch (Exception e) {
+//             System.err.println("Error calling the API: " + e.getMessage());
+//         }
+//     }
 
+//     @Async
+//     @Scheduled(fixedRate = 20000)
+//     public void callApiEvery45Minutesd() {
 
-    @Scheduled(fixedRate = 20000)
-    public void callApiEvery45Minutesd() {
-        String url = "http://192.168.200.10/pushservice/casepush.php"; // Replace with your API URL
-        try {
-            String response = restTemplate.getForObject(url, String.class);
-            System.out.println("API response: " + response);
-        } catch (Exception e) {
-            System.err.println("Error calling the API: " + e.getMessage());
-        }
-    }
-}
+//         String url = "http://192.168.200.10/pushservice/casepush.php"; // Replace with your API URL
+//         try {
+//             String response = restTemplate.getForObject(url, String.class);
+//             System.out.println("API response: " + response);
+//             Thread.sleep(3000);
+//         } catch (Exception e) {
+//             System.err.println("Error calling the API: " + e.getMessage());
+//         }
+//     }
+// }
